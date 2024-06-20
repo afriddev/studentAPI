8e9887d11cc67bb691a2f4f1854bad1f09715722
SHIAK AFRID <afridayan01@gmail.com>
2024-06-20 11:26:43 +0000

    modified home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 'use client'
-import connectDB from "@/libs/mongoDb/mongoDb"
-import { useEffect } from "react"
+import Link from "next/link";
+import { FaGithubAlt } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main>
       <section>
-        <div className="flex w-[100vw] h-[100vh] justify-center items-center">
-          <label className=" flexjustify-center items-center text-white text-2xl px-10 py-2 bg-blue-500 text font-mono rounded-lg shadow-xl">Student Api</label>
+        <div className="flex flex-col  gap-10 w-[100vw] h-[100vh] justify-center items-center">
+          <label className="text-sm max-w-[50vw] text-center">StudentsAPI will helps you in your developement for testing and other purposes</label>
+        <div className="flex items-center gap-2">
+        <FaGithubAlt />
+        <Link href={"https://github.com/afriddev/studentAPI/blob/main/README.md"}  className="text-xs border-b border-slate-800">Read docs</Link>
         </div>
+        
+        </div>
+
+
 
       </section>
     </main>
