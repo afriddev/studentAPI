79793463b4073c2f1136dad31a13d4bfe4264f45
SHIAK AFRID <afridayan01@gmail.com>
2024-10-19 13:37:44 -0400

    Update route.ts

--- app/api/allStudents/route.ts
+++ app/api/allStudents/route.ts
@@ -1,7 +1,6 @@
 import connectDB from "@/libs/mongoDb/mongoDb";
 import studentModel from "@/models/student";
 import { NextResponse } from "next/server";
-export const revalidate = 0; //must
 export async function GET() {
     try {
         await connectDB()
